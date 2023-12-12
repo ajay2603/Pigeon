180a2c961585ecb99e57f3c21975e16976577576
ajay2603 <j.a.r.raju2003@gmail.com>
2023-12-12 17:29:37 +0530

    NavBar added just a design without any functionality.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="h-full w-full p-2 bg-[#eff6fc] flex max-md:flex-col">
+      <div className="md:h-full md:w-fit max-md:w-full max-md:h-fit">
+        <NavBar />
+      </div>
+      <div className="md:w-full max-md:h-full"></div>
+    </div>
+  );
 }
 
 export default Home;
