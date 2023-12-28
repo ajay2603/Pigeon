25caa7b15d0f304032f4fd9788aa0a6d33b71412
ajay2603 <j.a.r.raju2003@gmail.com>
2023-12-28 18:14:09 +0530

    added env var.

--- backend/src/const.js
+++ backend/src/const.js
@@ -1,6 +1,5 @@
+require("dotenv").config();
 const { model } = require("mongoose");
-const dbConnector =
-  "mongodb://127.0.0.1:27017/pegionDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1";
-
+const dbConnector = process.env.DB_CONNECTOR;
 const clientUrl = "http://localhost:5173";
 module.exports = { dbConnector, clientUrl };
