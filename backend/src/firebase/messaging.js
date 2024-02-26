df442a5cd76e116200b8366d47b8cd5b42f4878a
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-26 17:36:14 +0530

    doen error

--- backend/src/firebase/messaging.js
+++ backend/src/firebase/messaging.js
@@ -1,10 +1,13 @@
 var admin = require("firebase-admin");
 
 function initFireBase() {
+  console.log("hello");
+  console.log(process.env.FB_SERVICE_KEY);
   var serviceAccount = require(process.env.FB_SERVICE_KEY);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("done");
 }
 
 function getFBAdmin() {
